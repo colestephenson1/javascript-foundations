ef12bfe264f61f5c37694643d8b9fd77601dbfa5
Cole Stephenson <colestephenson94@gmail.com>
2022-05-17 20:20:33 -0600

    Finish Unicorn

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -2,7 +2,18 @@ class Unicorn {
   constructor(name, color) {
     this.name = name;
     this.color = color;
+    if (this.color === undefined) {
+      this.color = 'white'
+    } else {
+    this.color = color;
   }
+} isWhite() {
+  if (this.color !== 'white') {
+    return false;
+  }
+} says(stuff) {
+  return `**;* ${stuff} *;**`
 }
+};
 
 module.exports = Unicorn;
