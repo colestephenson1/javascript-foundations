6461c1070cb96ae221131a688d7a1315657a15fd
Cole Stephenson <colestephenson94@gmail.com>
2022-05-18 06:42:42 -0600

    Finish Dragon

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,27 @@
+class Dragon {
+constructor(name, rider, isHungry) {
+this.name = name;
+this.rider = rider;
+this.meals = 0;
+this.hungry = isHungry
+if (this.hungry === undefined) {
+  this.hungry = true
+};
 
+} greet() {
+ return `Hi, ${this.rider}!`
+
+} eat() {
+  this.meals++;
+  if (this.meals < 3) {
+    this.hungry = true
+  } else {
+    this.hungry = false
+    return this.hungry
+  };
+};
+};
+
+
+
+module.exports = Dragon;
