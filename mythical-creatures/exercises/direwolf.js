0fb26d2445be170ba393d8d6a2f46bf91efac74e
Cole Stephenson <colestephenson94@gmail.com>
2022-06-07 07:48:19 -0600

    Finish Direwolf

--- mythical-creatures/exercises/direwolf.js
+++ mythical-creatures/exercises/direwolf.js
@@ -5,17 +5,18 @@ this.home = home || 'Beyond the Wall';
 this.size = size || 'Massive';
 this.starksToProtect = [];
 this.huntsWhiteWalkers = true;
-  }
+};
   protect(stark) {
     if(this.home === stark.location && this.starksToProtect.length < 2) {
-      stark.safe = true
-      this.huntsWhiteWalkers = false
-      this.starksToProtect.push(stark)
-    }
-}
+      stark.safe = true;
+      this.huntsWhiteWalkers = false;
+      this.starksToProtect.push(stark);
+    };
+};
   leave(stark) {
     stark.safe = false
     this.starksToProtect.pop(stark)
-  }
-}
+  };
+};
+
 module.exports = Direwolf;
--- mythical-creatures/exercises/stark.js
+++ mythical-creatures/exercises/stark.js
@@ -1,19 +1,24 @@
+const Direwolf = require('../exercises/direwolf');
+
 class Stark {
   constructor(name) {
 this.name = name.name;
 this.location = name.area || 'Winterfell';
 this.safe = false;
-  }
+};
   sayHouseWords() {
     if (this.safe === true) {
-      return 'The North Remembers'
+      return 'The North Remembers';
     } else {
-    return 'Winter is Coming'
-  }
-}
+    return 'Winter is Coming';
+  };
+};
   callDirewolf(name, home) {
-    var calledWolf = new Direwolf(name, 'Riverlands')
-    return calledWolf
-  }
-}
+    var calledWolf = new Direwolf(name, home);
+    calledWolf.home = this.location;
+    calledWolf.protect(this);
+    return calledWolf;
+  };
+};
+
 module.exports = Stark;
