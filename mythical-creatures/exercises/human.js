8d0722fdebb17e5512b51ccb96f7b85622fb4422
Cole Stephenson <colestephenson94@gmail.com>
2022-06-08 15:08:19 -0600

    Refactor Ogre

--- mythical-creatures/exercises/human.js
+++ mythical-creatures/exercises/human.js
@@ -4,15 +4,15 @@ class Human {
 this.name = name;
 this.encounterCounter = 0;
 this.knockedOut = false;
-  }
+};
   noticesOgre() {
     if (this.encounterCounter === 3 || this.encounterCounter === 6) {
       return true
     } else {
       return false
-    }
-  }
-}
+    };
+  };
+};
 
 
 module.exports = Human;
--- mythical-creatures/exercises/ogre.js
+++ mythical-creatures/exercises/ogre.js
@@ -3,23 +3,22 @@ class Ogre {
 this.name = name.name;
 this.home = name.abode || 'Swamp';
 this.swings = 0
-
-  }
+};
   encounter(human) {
     human.encounterCounter++
     if (human.noticesOgre() === true) {
       this.swingAt(human)
-    }
-  }
+    };
+  };
   swingAt(human) {
     this.swings++
     if (this.swings === 2) {
       human.knockedOut = true
-    }
-    }
+    };
+  };
   apologize(human) {
     human.knockedOut = false
-  }
   };
+};
 
 module.exports = Ogre;
